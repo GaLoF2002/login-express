601a3d52e35a3c5725a6a73a457887c7bc7841dd
GaloAndresE <galo.estrella.jimenez@udla.edu.ec>
2025-01-31 22:21:50 -0500

    Desactivando análisis automático y corrigiendo configuración de SonarCloud

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,7 +7,7 @@ app.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK' });
 });
 
-test('Debe responder con status OK', async () => {
+test('Debe responder  con status OK', async () => {
     const response = await request(app).get('/health');
     expect(response.status).toBe(200);
 });
